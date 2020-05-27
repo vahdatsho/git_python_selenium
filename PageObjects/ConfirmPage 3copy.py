e631fca237f2fb3b2f7da984628952dbffd82f18
vahdat <vahdatsho@gmail.com>
2020-05-27 07:12:17 -0700

    Adding 3 new file

---
+++ PageObjects/ConfirmPage 3copy.py
@@ -0,0 +1,47 @@
+# IGNORE EVERYTHING
+
+# DUMMY CODE FOR GITHUB
+
+
+class ConfirmPage:
+
+    def __init__(self, driver):
+
+        self.driver = driver
+
+
+
+    shop = (By.CSS_SELECTOR, "a[href*='shop']")
+    name = (By.CSS_SELECTOR, "[name='name']")
+
+    def shopitems(self):
+        return self.driver.find_element()
+
+    def get_name(self):
+        return self.driver.find_element()
+        
+        
+        
+class Git:
+    
+    
+    def __init__(self, driver):
+
+    self.driver = driver
+    
+    
+    some_method():
+        return Git
+
+
+shop = (By.CSS_SELECTOR, "a[href*='shop']")
+name = (By.CSS_SELECTOR, "[name='name']")
+dssdgsdgs
+def shopitems(self):
+    return self.driver.find_element()
+sgsdgsd
+def get_name(self):
+    return self.driver.find_element()
+    
+
+sdgsdgsdg
---
+++ PageObjects/ConfirmPage 4copy.py
@@ -0,0 +1,47 @@
+# IGNORE EVERYTHING
+
+# DUMMY CODE FOR GITHUB
+
+
+class ConfirmPage:
+
+    def __init__(self, driver):
+
+        self.driver = driver
+
+
+
+    shop = (By.CSS_SELECTOR, "a[href*='shop']")
+    name = (By.CSS_SELECTOR, "[name='name']")
+
+    def shopitems(self):
+        return self.driver.find_element()
+
+    def get_name(self):
+        return self.driver.find_element()
+        
+        
+        
+class Git:
+    
+    
+    def __init__(self, driver):
+
+    self.driver = driver
+    
+    
+    some_method():
+        return Git
+
+
+dfs
+ shop = (By.CSS_SELECTOR, "a[href*='shop']")
+ name = (By.CSS_SELECTOR, "[name='name']")
+dsfd
+ def shopitems(self):
+     return self.driver.find_element()
+sdf
+ def get_name(self):
+     return self.driver.find_element()
+     
+sdf
