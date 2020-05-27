b5ea65ff0317f1cdbffccbf25466626ecec2eedd
vahdat <vahdatsho@gmail.com>
2020-05-27 06:26:41 -0700

    This is commit from Vahdat

--- PageObjects/ConfirmPage.py
+++ PageObjects/ConfirmPage.py
@@ -19,3 +19,16 @@ class ConfirmPage:
 
     def get_name(self):
         return self.driver.find_element()
+        
+        
+        
+class Git:
+    
+    
+    def __init__(self, driver):
+
+    self.driver = driver
+    
+    
+    some_method():
+        return Git
